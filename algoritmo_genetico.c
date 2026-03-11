b4eb7783b98fb6e9a5680d5671c3bb369e130658
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-03-11 10:20:07 -0300

    Inicio do processo de algoritmo genético.

---
+++ algoritmo_genetico.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    //função para ler entrada de dados de input.dat
+    int ler_dados_arquivo(arquivo){
+        
+    }
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+#include <stdlib.h>
+
+int main(){
+    
+
+
+
+    return 0;
+}
