994d49826d6d4e41064dd049503d7e3cf85ee766
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-03-25 15:17:56 -0300

    Função calcular erro finalizada (sem teste).

--- calculos.c
+++ calculos.c
@@ -2,3 +2,29 @@
 #include <stdlib.h>
 
 // função para realizar os cálculos do algoritmo genético
+float funcao_reta(float a, float b, float x){
+    float y = (a * x) + b;
+    return y;
+}
+
+
+float calcular_erro_MAE_individuo(DadosEntrada *dadosEntrada, Individuo *populacao, int j){
+    float a = populacao[j].a;
+    float b = populacao[j].b;
+
+    float erro_total = 0.0;
+
+    for(int i=0;i<dadosEntrada->n;i++){
+        float y_funcao = funcao_reta(a, b, dadosEntrada->pontos[i].x);
+        float erro = y_funcao - dadosEntrada->pontos[i].y;
+
+        if(erro < 0){
+            erro *= -1;
+        }
+
+        erro_total += erro;
+    }
+
+    float media_erro_reta = erro_total / dadosEntrada->n;
+    return media_erro_reta;
+}
--- calculos.h
+++ calculos.h
@@ -4,7 +4,8 @@
 // todas as funções que envolvem cálculos estarão aqui, desconsidere o retorno e os parâmetros, a ideia é apenas
 // organizar o que ficará em cada módulo, no desenvolvimento esses aspectos serão resolvidos.
 
-void calcular_erro_MAE_individuo();
+float funcao_reta(float a, float b, float x);
+float calcular_erro_MAE_individuo(DadosEntrada *dadosEntradaentrada, Individuo *populacao, int j);
 void calcular_fitness();
 
 #endif /* CALCULOS_H */
